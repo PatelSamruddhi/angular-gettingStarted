ea3fcfab8348b4d860fd88c6093aea73b221a190
samruddhipatel <samruddhibpatel@gmail.com>
2020-12-23 16:22:27 +0100

    updated product list component and fixed imports

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
  selector: 'pm-root',
-templateUrl: './app.component.html'
+  template: `
+  <pm-products></pm-products>
+`,
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
--- APM-Start/src/app/home/welcome.component.ts
+++ APM-Start/src/app/home/welcome.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'pm-welcome-page',
   templateUrl: './welcome.component.html'
 })
 export class WelcomeComponent {
